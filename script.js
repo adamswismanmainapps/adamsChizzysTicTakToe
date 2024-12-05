b4a551838a60a4bb182a78a62b3d14e66a537bb2
adamswismanmainapps <adamsmainapps@gmail.com>
2024-12-05 10:01:27 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const Game = {
     firstUserLoss: 0,
     secondUserWin: 0,
     secondUserLoss: 0,
-    gamePlayGrid: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    gamePlayGrid: [null, null, null, null, null, null, null, null, null]
 }
 class TicTacToe{
     
     constructor(){
-        
+        document.getElementById("close").addEventListener("click", ()=>{
+            document.getElementById("wonView").style.display = "none"
+        })
         this.initializeCellBlockCilckCallBack();
     }
     initializeCellBlockCilckCallBack(){
@@ -37,12 +39,102 @@ class TicTacToe{
                 }
                 
                 this.initializeWinEventListner();
+                this.initializeDrawEventListner();
             })
         });
     }
     initializeWinEventListner(){
+        let winState = false;
+
+        if((Game.gamePlayGrid[0]==Game.gamePlayGrid[1]&&Game.gamePlayGrid[1]==Game.gamePlayGrid[2]) && Game.gamePlayGrid[2]!==null){
+            winState = true;
+        }
+        if((Game.gamePlayGrid[3]==Game.gamePlayGrid[4]&&Game.gamePlayGrid[4]==Game.gamePlayGrid[5]) && Game.gamePlayGrid[5]!==null){
+            winState = true;
+        }
+        if((Game.gamePlayGrid[6]==Game.gamePlayGrid[7]&&Game.gamePlayGrid[7]==Game.gamePlayGrid[8]) && Game.gamePlayGrid[8]!==null){
+            
+            winState = true;
+        }
+
+
+        if((Game.gamePlayGrid[0]==Game.gamePlayGrid[3]&&Game.gamePlayGrid[3]==Game.gamePlayGrid[6]) && Game.gamePlayGrid[6]!==null){
+
+            winState = true;
+        }
+        if((Game.gamePlayGrid[1]==Game.gamePlayGrid[4]&&Game.gamePlayGrid[4]==Game.gamePlayGrid[7]) && Game.gamePlayGrid[7]!==null){
+            winState = true;
+        }
+        if((Game.gamePlayGrid[2]==Game.gamePlayGrid[5]&&Game.gamePlayGrid[5]==Game.gamePlayGrid[8]) && Game.gamePlayGrid[8]!==null){
+            winState = true;
+        }
+
+
+        if((Game.gamePlayGrid[0]==Game.gamePlayGrid[4]&&Game.gamePlayGrid[4]==Game.gamePlayGrid[8]) && Game.gamePlayGrid[8]!==null){
+            winState = true;
+        }
+        if((Game.gamePlayGrid[2]==Game.gamePlayGrid[4]&&Game.gamePlayGrid[4]==Game.gamePlayGrid[6]) && Game.gamePlayGrid[6]!==null){
+            winState = true;
+        }
         
+
+        if(winState){
+            document.getElementById("wonText").innerHTML = `User #${Game.currentUser!=0?1:2} won the game`
+
+            document.getElementById("wonView").style.display = "flex";
+            switch(Game.currentUser){
+                case 1:
+                    Game.firstUserWin++;
+                    Game.secondUserLoss++;
+                    break;
+                case 0:
+                    Game.firstUserLoss++;
+                    Game.secondUserWin++
+                    break;
+            }
+            Game.currentUser = 0;
+            Game.gamePlayGrid = [null, null, null, null, null, null, null, null, null];
+            Game.gamePlayCount++;
+
+            let cellButtons = [...document.getElementsByClassName("cellBlock")]
+            cellButtons.forEach(element => {
+            element.innerHTML = "";
+        });
+        this.updateGuiText();
+
+        }
+        
+
     }
+    initializeDrawEventListner(){
+        if(!Game.gamePlayGrid.includes(null)){
+            document.getElementById("wonText").innerHTML = `Game is a Draw!`
+
+            document.getElementById("wonView").style.display = "flex";
+            Game.currentUser = 0;
+            Game.gamePlayGrid = [null, null, null, null, null, null, null, null, null];
+            Game.gamePlayCount++;
+
+            let cellButtons = [...document.getElementsByClassName("cellBlock")]
+            cellButtons.forEach(element => {
+            element.innerHTML = "";
+        });
+        }   
+    }
+    updateGuiText(){
+        
+        let guiFooterText = `<pre>
+ User #1 Wins: ${Game.firstUserWin}
+ User #2 Wins: ${Game.secondUserWin}
+        </pre>
+        <pre>
+            User #1 Loss: ${Game.firstUserLoss}
+            User #2 Loss: ${Game.secondUserLoss}
+        </pre>`
+        console.log(guiFooterText)
+        document.getElementById("scoreLine").innerHTML = guiFooterText;
+    }
+    
     
     
 }
